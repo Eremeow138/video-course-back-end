6a386b4ac81ff5ebfc0d8f2ed06e4b1424499868
Eremeow138 <eremeow138@yandex.ru>
2023-01-12 07:51:51 +0300

    fix: change port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const init = require('./services');
 const walk = require('./utils/walk');
 const cors = require('./utils/cors');
 
+const port = 3000;
+
 let ang;
 
 walk('./services', function (err, results) {
@@ -12,19 +14,19 @@ walk('./services', function (err, results) {
 		console.log(err);
 	} else {
 		ang = init(results);
-		
+
 		server.use(cors);
-		
+
 		server.use(jsonServer.bodyParser);
 		server.use(middleware);
-		
+
 		//
 		server.use(ang.routes);
 		server.use(ang.middleware);
 		server.use(ang.db);
-		
-		server.listen(3004, function () {
-			console.log('JSON Server is running on 3004');
+
+		server.listen(port, function () {
+			console.log(`JSON Server is running on ${port}`);
 		});
 	}
 });
